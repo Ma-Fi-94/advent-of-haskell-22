8b1fe40ee143ae5510cee45a1ca896d6f6a65c2c
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2025-01-02 23:50:32 +0100

    Part 1, but not yet sub-second
    ~2.1s -- switching from lists of Int to some Range type seems appropriate.

---
+++ day15/Main.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import Data.List ((\\))
+import Utils (countUnique, readInt, tok)
+
+type Coord   = (Int, Int)
+type Sensor  = Coord
+type Beacon  = Coord
+type Reading = (Sensor, Beacon)
+
+-- Parse an input line into a Reading object.
+parseLine     :: String -> Reading
+parseLine line = ((nbs !! 0, nbs !! 1), (nbs !! 2, nbs !! 3))
+  where
+    nbs = map readInt . tok "=" . filter (`elem` "0123456789=-") $ line
+
+-- Taxicab distance between two points.
+cab                  :: Coord -> Coord -> Int
+cab (x1, y1) (x2, y2) = abs (x2 - x1) + abs (y2 - y1)
+
+-- Given a line number and one reading, calculate which columns in that
+-- row are free. Note that we need to exclude the point of the beacon
+-- if the beacon lies on the line we examine.
+findFreeCols                        :: Int -> Reading -> [Int]
+findFreeCols y ((sx, sy), (bx, by)) = if   y == by
+                                      then xs \\ [bx]
+                                      else xs
+  where
+    r     = cab (sx, sy) (bx, by)
+    dy    = abs (y - sy)
+    dxMax = r - dy
+    xs    = [(sx - dxMax) .. (sx + dxMax)]
+
+main :: IO ()
+main = do
+    readings <- map parseLine . lines <$> readFile "input.txt"
+
+    -- Part 1
+    print $ countUnique . concatMap (findFreeCols 2000000) $ readings
+
+    print $ "Done."
+
