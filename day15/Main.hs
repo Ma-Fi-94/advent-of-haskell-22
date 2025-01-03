9f721c54aa4fffeaa9af3aaf79c4c160ab770c1c
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2025-01-03 22:58:37 +0100

    Part 2 done, but not subsecond yet
    (average of 6s for a naive brute-force approach)

--- day15/Main.hs
+++ day15/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Control.Arrow (second)
+import Data.List (sort)
 import Range (Range(..))
 import qualified Range as R
-import Utils (readInt, tok)
+import Utils (readInt, tok, map2)
 
 type Coord   = (Int, Int)
 type Sensor  = Coord
@@ -19,29 +21,69 @@ parseLine line = ((nbs !! 0, nbs !! 1), (nbs !! 2, nbs !! 3))
 cab                  :: Coord -> Coord -> Int
 cab (x1, y1) (x2, y2) = abs (x2 - x1) + abs (y2 - y1)
 
--- Given a line number and one reading, calculate which columns in that
+-- The number of elements in an Int Range
+nbElems :: Range Int -> Int
+nbElems (R a b)
+    | a <= b    = b - a + 1
+    | otherwise = a - b + 1
+
+-- Given a line number and ONE reading, calculate which columns in that
 -- row are free. Note that we need to exclude the point of the beacon
 -- if the beacon lies on the line we examine.
-findFreeCols                        :: Int -> Reading -> [Range Int]
-findFreeCols y ((sx, sy), (bx, by)) = if   y == by
-                                      then bx `R.remove` R (sx - dxMax) (sx + dxMax)
-                                      else [R (sx - dxMax) (sx + dxMax)]
+findFreeCols :: Int -> Reading -> [Range Int]
+findFreeCols y ((sx, sy), (bx, by))
+    | dxMax <= 0 = []
+    | y == by    = bx `R.remove` R (sx - dxMax) (sx + dxMax)
+    | otherwise  = [R.range (sx - dxMax) (sx + dxMax)]
   where
     r     = cab (sx, sy) (bx, by)
     dy    = abs (y - sy)
     dxMax = r - dy
+
+------------
+-- Part 2 --
+------------
+
+-- We now need a slightly different version of findFreeCols, where
+-- points with beacons are not excluded, since the distressed beacon
+-- can not stand on these.
+findFreeCols' :: Int -> Reading -> [Range Int]
+findFreeCols' y ((sx, sy), (bx, by))
+    | dxMax <= 0 = []
+    | otherwise  = [R.range (sx - dxMax) (sx + dxMax)]
+  where
+    r     = cab (sx, sy) (bx, by)
+    dy    = abs (y - sy)
+    dxMax = r - dy
+
+-- Given a List of DISJUNCT Int Ranges, find all Gaps between them.
+findGaps :: [Range Int] -> [Int]
+findGaps ranges = go (sort ranges)
+  where
+    go ((R a b) : [])           = []
+    go ((R a b) : (R c d) : rs) = [(b + 1) .. (c - 1)] ++ go ((R c d) : rs)
 
 main :: IO ()
 main = do
     readings <- map parseLine . lines <$> readFile "input.txt"
 
 
-    -- Part 1
-    let allRanges = concatMap (findFreeCols 2000000) readings
+    -- Part 1: Only examine row 10 in particular.
     print . sum
-          . map (\ (R a b) -> b - a + 1)
-          $ foldl R.disjunctJoin [] allRanges
+          . map nbElems
+          . R.disjunctify
+          . concatMap (findFreeCols 10)
+          $ readings
 
+    -- Part 2: Like Part 1, but we examine all rows from 0 to 4000000.
+    print . id -- (\ (y, x) -> 4000000 * x + y)
+          . head
+          . map (second head)
+          . filter (\ (y, xs) -> xs /= [])
+          . map (second findGaps)
+          . map (second R.disjunctify)
+          . map (\ y -> (y, concatMap (findFreeCols' y) readings))
+          $ [0..4000000]
 
 
     print $ "Done."
